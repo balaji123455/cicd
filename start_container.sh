737b89565d8390b528f3d40699ad1f0c60a08ad8
balaji123455 <balajigan2002@gmail.com>
2025-03-28 08:00:36 +0000

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull nithish164/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 6000:9000 nithish164/simple-python-flask-app:latest 
